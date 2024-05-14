ba07c8098c78b190450b4af277eaad48c410d9fd
Fabio Correa Gomes <fabiocorreagomes@gmail.com>
2024-05-14 08:17:28 -0300

    feat: new folder structure

--- src/components/Pokemon.jsx
+++
@@ -1,5 +0,0 @@
-const Pokemon = ( { name = 'Pikachu' } ) => {
-    return <h2>Nome do Pokemon: {name}</h2>
-  }
-
-  export default Pokemon;
---
+++ src/components/Pokemon/index.jsx
@@ -0,0 +1,5 @@
+const Pokemon = ( { name = 'Pikachu' } ) => (
+    <h2>Nome do Pokemon: {name}</h2>
+  )
+
+  export default Pokemon;
